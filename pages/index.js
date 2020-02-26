287187b7f178f1bca43236492bafee916663d9ee
wangsq <358009775@qq.com>
2020-02-26 19:51:41 +0800

    hello next.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Hello Next.js</p>
+            </div>
+        );
+    }
+}
+
+export default Index;
